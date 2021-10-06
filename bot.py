003fd2192b1175eae2f313cf72235a8b0f5d1f76
cusci <79975885+cusci@users.noreply.github.com>
2021-10-07 01:01:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,7 +90,7 @@ async def home(client, message):
   reply_markup = InlineKeyboardMarkup(buttons)
   await Tgraph.send_message(
         chat_id=message.chat.id,
-        text="""<b>👋 Hey ciao    
+        text="""<b>👋 Hey ciao {m.from_user.mention}    
 👉🏻 Invia un media per ricevere il link Telegra.ph        
 💥 Bot By @cusciproject</b>""",
         reply_markup=reply_markup,
